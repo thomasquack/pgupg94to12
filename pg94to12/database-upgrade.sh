2d7a1ee11bdb08206ac36fd6968ed83274faeb12
geraldyong <geraldyong@data.gov.sg>
2020-05-24 10:44:51 +0800

    Converted to v9.4

--- pg94to12/database-upgrade.sh
+++ pg94to12/database-upgrade.sh
@@ -3,9 +3,9 @@
 set -e
 
 # Perform the database upgrade.
-BIN96=/usr/lib/postgresql/9.6/bin
+BIN94=/usr/lib/postgresql/9.4/bin
 BIN12=/usr/lib/postgresql/12/bin
-DATA96=/var/lib/postgresql/9.6/main
+DATA94=/var/lib/postgresql/9.4/main
 DATA12=/var/lib/postgresql/12/main
 
 echo "Current User changing data owner to postgres user"
@@ -15,8 +15,8 @@ sudo -i -u postgres bash << EOF
 echo "Current User to update datasebase"
 whoami
 cd /tmp
-${BIN12}/pg_upgrade --old-bindir ${BIN96} --new-bindir ${BIN12} --old-datadir ${DATA96} --new-datadir ${DATA12} \
--o '-c config_file=${DATA96}/postgresql.conf' -O '-c config_file=${DATA12}/postgresql.conf'
-cp -p ${DATA96}/pg_hba.conf ${DATA12}/pg_hba.conf
+${BIN12}/pg_upgrade --old-bindir ${BIN94} --new-bindir ${BIN12} --old-datadir ${DATA94} --new-datadir ${DATA12} \
+-o '-c config_file=${DATA94}/postgresql.conf' -O '-c config_file=${DATA12}/postgresql.conf'
+cp -p ${DATA94}/pg_hba.conf ${DATA12}/pg_hba.conf
 echo "Database upgrade completed."
 EOF
